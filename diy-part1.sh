3a700e8850ab908972a8358a7d7578d95568b307
rizkikotet-dev <rizkidhc31@gmail.com>
2025-07-07 13:54:11 +0700

    Remove helloworld feed from diy-part1.sh and add universal.config with comprehensive configuration settings for various packages, kernel options, and build optimizations.

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,6 @@
 # diy-part1.sh - Kustomisasi feeds dan tambahan repository
 
 # Menambahkan feed tambahan
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
