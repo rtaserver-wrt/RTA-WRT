6142d4a41e900c3653a20d919f9dfd27364ce52e
rizkikotet-dev <rizkidhc31@gmail.com>
2025-07-07 18:33:19 +0700

    Add additional feed for passwall-packages to feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,5 +3,6 @@
 
 # Menambahkan feed tambahan
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+echo "src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages" >> feeds.conf.default
 echo "src-git openclash https://github.com/vernesong/OpenClash" >> feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
